61bb74955f728d9f96f1c81c33d19f8ba5ef695c
Ilya Stritskiy <msv_ilya@mail.ru>
2024-11-30 23:23:01 +0300

    webpack prod configuration has been created

---
+++ prod-webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+
+module.exports = {
+    mode: 'production',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist/prod-dist'),
+        filename: 'prod-bundle.js'
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'prod-style.css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'prod-index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {esModule: true}
+                    },
+                    'css-loader',
+                ]
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            new TerserPlugin(),
+            new CssMinimizerPlugin()
+        ]
+    }
+    
+}
